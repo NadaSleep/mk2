2f2cd15c18364449772009ec3f33ebf1dabec95f
Tyler Sexton <tyler.m.sexton@gmail.com>
2020-07-27 08:43:57 -0400

    Fixed issue with character info changing and also started new page to spread out content

---
+++ js/characterGrid.js
@@ -0,0 +1,48 @@
+function liuKang() {
+    document.getElementById('characterInfo').style.backgroundImage = "url(images/Characters/info/kangBio.jpg)";   
+}
+
+function kunglao() {
+    document.getElementById('characterInfo').style.backgroundImage = "url(images/Characters/info/laoBio.jpg)";   
+}
+
+function johnnycage() {
+    document.getElementById('characterInfo').style.backgroundImage = "url(images/Characters/info/cageBio.jpg)";   
+}
+
+function reptile() {
+    document.getElementById('characterInfo').style.backgroundImage = "url(images/Characters/info/reptileBio.jpg)";   
+}
+
+function shangtsung() {
+    document.getElementById('characterInfo').style.backgroundImage = "url(images/Characters/info/tsungBio.jpg)";   
+}
+
+function subzero() {
+    document.getElementById('characterInfo').style.backgroundImage = "url(images/Characters/info/subzeroBio.jpg)";   
+}
+
+function katana() {
+    document.getElementById('characterInfo').style.backgroundImage = "url(images/Characters/info/kitanaBio.jpg)";   
+}
+
+function jaxbriggs() {
+    document.getElementById('characterInfo').style.backgroundImage = "url(images/Characters/info/jaxBio.jpg)";   
+}
+
+function mileena() {
+    document.getElementById('characterInfo').style.backgroundImage = "url(images/Characters/info/mileenaBio.jpg)";   
+}
+
+function baraka() {
+    document.getElementById('characterInfo').style.backgroundImage = "url(images/Characters/info/barakaBio.jpg)";   
+}
+
+function scorpion() {
+    document.getElementById('characterInfo').style.backgroundImage = "url(images/Characters/info/scorpionBio.jpg)";   
+}
+
+function raiden() {
+    document.getElementById('characterInfo').style.backgroundImage = "url(images/Characters/info/raidenBio.jpg)";   
+}
+
